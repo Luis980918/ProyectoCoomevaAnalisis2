68c9a74db5c2bb606d34744fca257c60011ce360
Luis980918 <55850466+Luis980918@users.noreply.github.com>
2020-04-19 02:04:28 -0500

    Revert "Se agregaron las clases de Rol y Tservicios y sus equivalentes"
    This reverts commit 0b4b585fcd5e2c21bb2b4ab7d7455bb73f4c4e97.

--- src/main/java/com/udea/analisis/rtf/repository/TServicioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.udea.analisis.rtf.repository;
-import com.udea.analisis.rtf.entity.TServicio;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TServicioRepository extends CrudRepository<TServicio, Long> {
-}
